8f20f904f38e8881fbaf50fc0c3ace41aa14db54
zilvi20 <zilvi20@students.cs.ubc.ca>
2023-10-30 15:10:45 -0700

    Working with correct looks

--- src/ai_system.cpp
+++ src/ai_system.cpp
@@ -21,8 +21,8 @@ vec2 find_map_index(vec2 pos) {
 
 vec2 find_index_from_map(vec2 pos) {
 	//printf("before: %f, %f==========", pos.x, pos.y);
-	pos.x = (int)pos.x * RATIO_WIDTH + OFFSET_WIDTH;
-	pos.y = (int)pos.y * RATIO_HEIGHT - OFFSET_HEIGHT;
+	pos.x = (pos.x + 1.f) * RATIO_WIDTH + OFFSET_WIDTH;
+	pos.y = (pos.y + 1.f) * RATIO_HEIGHT - OFFSET_HEIGHT;
 	//printf("AFTER: %f, %f\n", pos.x, pos.y);
 
 	return pos;
@@ -32,31 +32,33 @@ std::list<vec2> dfs_follow_helper(std::vector<std::vector<char>>& vec, vec2 pos,
 	if (vec[pos.y][pos.x] == 'g') return path;
 	path.push_front(vec2(pos));
 	vec[pos.y][pos.x] = 'v';
-	std::list<vec2> ret_path(300, vec2(1, 1));
-	std::list<vec2> temp_path(300, vec2(1, 1));
+	std::list<vec2> ret_path(300, vec2(-1, -1));
+	std::list<vec2> temp_path(300, vec2(-1, -1));
 
 	//visit right
-	if (pos.x + 1.f < vec[0].size() && pos.x - 1 >= 0 && vec[pos.y][pos.x + 1.f] != 'v' && vec[pos.y][pos.x + 1.f] != 'b') {
+	if (pos.x + 1.f < vec[0].size() && vec[pos.y][pos.x + 1.f] != 'v' && vec[pos.y][pos.x + 1.f] != 'b') {
 		ret_path = dfs_follow_helper(vec, vec2(pos.x + 1.f, pos.y), path);
 	}
 	//visit left
-	if (pos.x - 1.f < vec[0].size() && pos.x - 1.f >= 0 && vec[pos.y][pos.x - 1.f] != 'v' && vec[pos.y][pos.x - 1.f] != 'b') {
+	if ( pos.x - 1.f >= 0 && vec[pos.y][pos.x - 1.f] != 'v' && vec[pos.y][pos.x - 1.f] != 'b') {
 		temp_path = dfs_follow_helper(vec, vec2(pos.x - 1.f, pos.y), path);
 	}
-	if (ret_path.size() == 0) ret_path = temp_path;
-	if (ret_path.size() > temp_path.size()) ret_path = temp_path;
+
+	if (ret_path.back().x == -1 || ret_path.size() > temp_path.size()) ret_path = temp_path;
+	
 	//visit up
-	if (pos.y - 1.f < vec.size() && pos.y - 1.f >= 0 && vec[pos.y - 1.f][pos.x] != 'v' && vec[pos.y - 1.f][pos.x] != 'b') {
+	if ( pos.y - 1.f >= 0 && vec[pos.y - 1.f][pos.x] != 'v' && vec[pos.y - 1.f][pos.x] != 'b') {
 		temp_path = dfs_follow_helper(vec, vec2(pos.x, pos.y - 1.f), path);
 	}
-	if (ret_path.size() == 0) ret_path = temp_path;
-	if (ret_path.size() > temp_path.size()) ret_path = temp_path;
+	
+	if (ret_path.back().x == -1 || ret_path.size() > temp_path.size()) ret_path = temp_path;
+	
 	//visit down
-	if (pos.y + 1.f < vec.size() && pos.y + 1.f >= 0 && vec[pos.y + 1.f][pos.x] != 'v' && vec[pos.y + 1.f][pos.x] != 'b') {
+	if (pos.y + 1.f < vec.size() && vec[pos.y + 1.f][pos.x] != 'v' && vec[pos.y + 1.f][pos.x] != 'b') {
 		temp_path = dfs_follow_helper(vec, vec2(pos.x, pos.y + 1.f), path);
 	}
-	if (ret_path.size() == 0) ret_path = temp_path;
-	if (ret_path.size() > temp_path.size()) ret_path = temp_path;
+
+	if (ret_path.back().x == -1 || ret_path.size() > temp_path.size()) ret_path = temp_path;
 
 	return ret_path;
 }
@@ -73,13 +75,13 @@ std::vector<std::vector<char>> create_grid() {
 	std::vector<std::vector<char> > vect(9, line);
 
 	std::vector<char> temp = { 'n', 'n', 'n', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'n', 'n', 'n'};
-	vect[2] = temp;
+	vect[1] = temp;
 	temp = { 'b', 'b', 'b', 'b', 'n', 'n', 'n', 'n', 'n', 'n', 'b', 'b', 'b', 'b' };
-	vect[4] = temp;
+	vect[3] = temp;
 	temp = {  'n', 'n', 'b', 'b', 'b', 'n', 'n', 'n', 'n', 'b', 'b', 'b', 'n', 'n' };
-	vect[6] = temp;
+	vect[5] = temp;
 	temp = {  'b', 'b', 'b', 'n', 'n', 'b', 'b', 'b', 'b', 'n', 'n', 'b', 'b', 'b' };
-	vect[8] = temp;
+	vect[7] = temp;
 
 	return vect;
 }
