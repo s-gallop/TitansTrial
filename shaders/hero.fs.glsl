814b62b48eeaeb15ca0f53b33c3fb2eea2981f6a
s-gallop <sgvitkunas@gmail.com>
2023-10-30 20:43:43 -0700

    Hero flashes when hit

--- shaders/hero.fs.glsl
+++ shaders/hero.fs.glsl
@@ -8,6 +8,8 @@ uniform vec2 scale;
 // Application data
 uniform sampler2D sampler0;
 uniform vec3 fcolor;
+uniform float invulnerable_timer;
+uniform float M_PI;
 
 // Output color
 layout(location = 0) out  vec4 color;
@@ -15,4 +17,7 @@ layout(location = 0) out  vec4 color;
 void main()
 {
 	color = vec4(fcolor, 1.0) * texture(sampler0, vec2((texcoord.x+frame.x)/scale.x, (texcoord.y+frame.y)/scale.y));
+	if (invulnerable_timer > 0) {
+		color *= vec4(0.3 + abs(cos(M_PI - M_PI * invulnerable_timer / 3000.f)) / 0.5, 0, 0, 1);
+	}
 }
